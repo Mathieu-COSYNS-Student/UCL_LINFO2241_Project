ee4f302c98437a8498f4999778b05085057f2c50
Mathieu-COSYNS-Student <mathieu.cosyns@student.uclouvain.be>
2021-12-20 22:05:23 +0100

    Change StringsIteratorTest form Visual to Edges

--- src/test/java/server/StringsIteratorTest.java
+++ src/test/java/server/StringsIteratorTest.java
@@ -2,6 +2,7 @@ package server;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
 import org.junit.Test;
 
 public class StringsIteratorTest {
@@ -24,12 +25,30 @@ public class StringsIteratorTest {
   }
 
   @Test
-  public void testVisual() {
+  public void testEdges() {
     StringsIterator stringsIterator = new StringsIterator(0, 3);
+    HashMap<Integer, String> positionValues = new HashMap<>();
+    positionValues.put(0, "");
+    positionValues.put(1, "a");
+    positionValues.put(26, "z");
+    positionValues.put(1 + 26, "aa");
+    positionValues.put(1 + 26 + 26 * 26 - 1, "zz");
+    positionValues.put(1 + 26 + 26 * 26, "aaa");
+    positionValues.put(1 + 26 + 26 * 26 + 26 * 26 * 26 - 1, "zzz");
 
+    int i = 0;
     while (stringsIterator.hasNext()) {
-      System.out.println(stringsIterator.next());
+      String item = stringsIterator.next();
+      String expected = positionValues.get(i);
+      if (expected != null) {
+        assertEquals("Iterator item n°" + i + " has not the expected value", expected, item);
+      }
+      i++;
     }
+
+    assertEquals("Iterator has not iterated over all supposed item",
+        1 + 26 + 26 * 26 + 26 * 26 * 26,
+        i);
   }
 
   @Test
