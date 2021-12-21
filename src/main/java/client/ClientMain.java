3824cbfd5d032747db5ac2a22e548d53b89a1e02
BrandonNgoranNtam <62793833+BrandonNgoranNtam@users.noreply.github.com>
2021-12-21 17:20:22 +0100

    Modified the Client so that we can get "constant" random value for the measurements

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -22,11 +22,12 @@ import javax.crypto.SecretKey;
 public class ClientMain {
 
     private static final Map<Sender, Long> connections = new HashMap<>();
+    private static final Random r = new Random(3);
 
 
     public static void main(String[] args) {
         try {
-            for (String password : getListOfPasswords()) {
+            for (String password : getRandomPasswordList()) { //This method will always give you the same list of 75 passwords
                 SecretKey keyGenerated = CryptoUtils.getKeyFromPassword(password);
 
                 File inputFile = getRandomFileToBeEncrypted();
@@ -39,9 +40,9 @@ public class ClientMain {
 
                 // This is an example to help you create your request
                 CryptoUtils.encryptFile(keyGenerated, inputFile, encryptedFile);
-                System.out.println("Password used: "+password);
-                System.out.println("Input file length of "+inputFile.getName()+" : " + inputFile.length());
-                System.out.println("Encrypted file length of "+encryptedFile.getName()+" : " + encryptedFile.length());
+                System.out.println("Password used: " + password);
+                System.out.println("Input file length of " + inputFile.getName() + " : " + inputFile.length());
+                System.out.println("Encrypted file length of " + encryptedFile.getName() + " : " + encryptedFile.length());
 
                 // SEND THE PROCESSING INFORMATION AND FILE
                 byte[] hashPwd = Hash.sha1(password);
@@ -103,13 +104,13 @@ public class ClientMain {
         return k - 1;
     }
 
-    private static List<String> getListOfPasswords() {
+    /*private static List<String> getListOfPasswords() {
         ArrayList<String> listOfPasswords = new ArrayList<>(Arrays.asList("alley", "lillie", "manga", "kinky",
-                        "schmidt", "dogboy", "billybob", "volvo", "perry",
-                        "sentra", "zurich", "forum", "safety", "candyass",
-                        "mailman", "smokie", "redskin", "punkrock", "anne",
-                        "malone", "fastball", "diane", "classics", "ccccccc",
-                        "sparky"));
+                "schmidt", "dogboy", "billybob", "volvo", "perry",
+                "sentra", "zurich", "forum", "safety", "candyass",
+                "mailman", "smokie", "redskin", "punkrock", "anne",
+                "malone", "fastball", "diane", "classics", "ccccccc",
+                "sparky"));
         int numberOfRandomPasswords = 50;
         for (int i = 1; i <= numberOfRandomPasswords; i++) {
             listOfPasswords.add(randomPasswordGenerator());
@@ -121,13 +122,27 @@ public class ClientMain {
     private static String randomPasswordGenerator() {
         String characters = "abcdefghijklmnopqrstuvwxyz";
         return RandomStringUtils.random(5, characters);
+    }*/
+
+    private static List<String> getRandomPasswordList() {
+        return new ArrayList<>(Arrays.asList("vrmeh", "oiwfm", "hhfsp", "alley", "redskin",
+                "billybob", "zvgeo", "qckwd", "sparky","punkrock", "ghihv", "jeqnb", "volvo",
+                "zurich", "jnggw", "ymmpa", "dfkyd","uihup", "mbabw", "trinh", "xgvgd",
+                "wgqyl", "pgkbh", "pqdqg", "forum","pjhek", "pgkiy", "sentra", "smokie",
+                "nbkij", "diane", "xqtfg", "schmidt","tcluo", "mailman", "ewddr", "aefqd",
+                "kinky", "sbhba", "zjpof", "lrfft", "inyqc","kngfg", "ybuxx", "amasz",
+                "fastball", "ccccccc","pszbw", "miffs", "qahfr", "malone", "bppmn", "anne",
+                "mesdr", "lpsok", "gvaox", "classics", "fmzsh", "blnlo", "lillie", "candyass",
+                "kkbnb", "umgtf", "lksty", "dogboy","qasiz", "manga", "safety", "saqfs",
+                "fnlbi", "nsene", "wgpwd", "ixumt", "perry", "pmrho"
+                ));
     }
 
     private static File getRandomFileToBeEncrypted() {
         File directory = new File("src/main/resources/filesToBeEncrypted");
         File[] files = directory.listFiles();
         if (files != null) {
-            int randomIndex = new Random().nextInt(files.length);
+            int randomIndex = r.nextInt(files.length);
             return files[randomIndex];
         }
         return null;
