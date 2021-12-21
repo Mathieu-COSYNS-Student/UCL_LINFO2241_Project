e33a65d8356b04150da2f57ec084efaa22eabf05
BrandonNgoranNtam <62793833+BrandonNgoranNtam@users.noreply.github.com>
2021-12-21 15:33:49 +0100

    Some adjustments to the ClientMain (Testing random passwords + different file sizes)

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -4,6 +4,8 @@ import common.CryptoUtils;
 import common.Hash;
 import common.Request;
 import common.Response;
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
@@ -11,8 +13,7 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
@@ -20,64 +21,117 @@ import javax.crypto.SecretKey;
 
 public class ClientMain {
 
-  private static final Map<Sender, Long> connections = new HashMap<>();
+    private static final Map<Sender, Long> connections = new HashMap<>();
 
 
-  public static void main(String[] args) {
-    try {
-      String password = "test";
-      SecretKey keyGenerated = CryptoUtils.getKeyFromPassword(password);
+    public static void main(String[] args) {
+        try {
+            for (String password : getListOfPasswords()) {
+                SecretKey keyGenerated = CryptoUtils.getKeyFromPassword(password);
 
-      File inputFile = new File("test_file.pdf");
-      File encryptedFile = new File("test_file-encrypted-client.pdf");
+                File inputFile = getRandomFileToBeEncrypted();
+                File encryptedFile;
+                if (inputFile != null) {
+                    encryptedFile = new File("encrypted-" + inputFile.getName());
+                } else {
+                    throw new NoSuchElementException();
+                }
 
-      // This is an example to help you create your request
-      CryptoUtils.encryptFile(keyGenerated, inputFile, encryptedFile);
-      System.out.println("Input file length: " + inputFile.length());
-      System.out.println("Encrypted file length: " + encryptedFile.length());
+                // This is an example to help you create your request
+                CryptoUtils.encryptFile(keyGenerated, inputFile, encryptedFile);
+                System.out.println("Password used: "+password);
+                System.out.println("Input file length of "+inputFile.getName()+" : " + inputFile.length());
+                System.out.println("Encrypted file length of "+encryptedFile.getName()+" : " + encryptedFile.length());
 
-      // SEND THE PROCESSING INFORMATION AND FILE
-      byte[] hashPwd = Hash.sha1(password);
-      int pwdLength = password.length();
-      long fileLength = encryptedFile.length();
-      // Creating socket to connect to server (in this example it runs on the localhost on port 3333)
-      Sender sender;
-      for (int i = 0; i < 100; i++) {
-        long startTime = System.currentTimeMillis();
-        Socket socket = new Socket("localhost", 3333);
-        Request request = new Request(hashPwd, pwdLength, fileLength, encryptedFile);
-        sender = new Sender(request, socket);
-        connections.put(sender, startTime);
-        sender.start();
-      }
-      int requestCounter = 1;
-      for (Sender connection : connections.keySet()) {
-        connection.join();
-        Response response = connection.getResponse();
-        if (response != null) {
-          //System.out.println("Decrypted file length from the server response: " + response.getFileLength());
-          //System.out.println("Decrypted file length: " + response.getFile().length());
-          long elapsedTime = System.currentTimeMillis() - connections.get(connection);
-          getFormattedTimeMeasurements(elapsedTime, requestCounter);
-        } else {
-          System.out.println("No response from the server");
+                // SEND THE PROCESSING INFORMATION AND FILE
+                byte[] hashPwd = Hash.sha1(password);
+                int pwdLength = password.length();
+                long fileLength = encryptedFile.length();
+                // Creating socket to connect to server (in this example it runs on the localhost on port 3333)
+                Sender sender;
+
+                Thread.sleep(getPoissonRandomNumber(0.06)); // 1 request every 15 seconds aka 1/15
+                long startTime = System.currentTimeMillis();
+                Socket socket = new Socket("localhost", 3333);
+                Request request = new Request(hashPwd, pwdLength, fileLength, encryptedFile);
+                sender = new Sender(request, socket);
+                connections.put(sender, startTime);
+                sender.start();
+            }
+
+
+            int requestCounter = 1;
+            for (Sender connection : connections.keySet()) {
+                connection.join();
+                Response response = connection.getResponse();
+                if (response != null) {
+                    //System.out.println("Decrypted file length from the server response: " + response.getFileLength());
+                    //System.out.println("Decrypted file length: " + response.getFile().length());
+                    long elapsedTime = System.currentTimeMillis() - connections.get(connection);
+                    getFormattedTimeMeasurements(elapsedTime, requestCounter);
+                } else {
+                    System.out.println("No response from the server");
+                }
+                requestCounter++;
+            }
+
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException | InvalidAlgorithmParameterException
+                | NoSuchPaddingException | IllegalBlockSizeException | IOException | BadPaddingException
+                | InvalidKeyException | InterruptedException e) {
+            e.printStackTrace();
         }
-        requestCounter++;
-      }
 
-    } catch (NoSuchAlgorithmException | InvalidKeySpecException | InvalidAlgorithmParameterException
-        | NoSuchPaddingException | IllegalBlockSizeException | IOException | BadPaddingException
-        | InvalidKeyException | InterruptedException e) {
-      e.printStackTrace();
     }
 
-  }
+    public static void getFormattedTimeMeasurements(long timeInMilliseconds, int senderID) {
+        long minutes = (timeInMilliseconds / 1000) / 60;
+        long seconds = (timeInMilliseconds / 1000) % 60;
+        System.out.println(
+                "Request n°" + senderID + " ---> Request/Response Time : " + minutes + " minutes and "
+                        + seconds + " seconds");
+    }
 
-  public static void getFormattedTimeMeasurements(long timeInMilliseconds, int senderID) {
-    long minutes = (timeInMilliseconds / 1000) / 60;
-    long seconds = (timeInMilliseconds / 1000) % 60;
-    System.out.println(
-        "Request n°" + senderID + " ---> Request/Response Time : " + minutes + " minutes and "
-            + seconds + " seconds");
-  }
+    private static int getPoissonRandomNumber(double rate) {
+        Random r = new Random();
+        double L = Math.exp(-rate);
+        int k = 0;
+        double p = 1.0;
+        do {
+            p = p * r.nextDouble();
+            k++;
+        } while (p > L);
+        return k - 1;
+    }
+
+    private static List<String> getListOfPasswords() {
+        ArrayList<String> listOfPasswords = new ArrayList<>(Arrays.asList("alley", "lillie", "manga", "kinky",
+                        "schmidt", "dogboy", "billybob", "volvo", "perry",
+                        "sentra", "zurich", "forum", "safety", "candyass",
+                        "mailman", "smokie", "redskin", "punkrock", "anne",
+                        "malone", "fastball", "diane", "classics", "ccccccc",
+                        "sparky"));
+        int numberOfRandomPasswords = 50;
+        for (int i = 1; i <= numberOfRandomPasswords; i++) {
+            listOfPasswords.add(randomPasswordGenerator());
+        }
+        Collections.shuffle(listOfPasswords);
+        return listOfPasswords;
+    }
+
+    private static String randomPasswordGenerator() {
+        String characters = "abcdefghijklmnopqrstuvwxyz";
+        return RandomStringUtils.random(5, characters);
+    }
+
+    private static File getRandomFileToBeEncrypted() {
+        File directory = new File("src/main/resources/filesToBeEncrypted");
+        File[] files = directory.listFiles();
+        if (files != null) {
+            int randomIndex = new Random().nextInt(files.length);
+            return files[randomIndex];
+        }
+        return null;
+
+
+    }
 }
